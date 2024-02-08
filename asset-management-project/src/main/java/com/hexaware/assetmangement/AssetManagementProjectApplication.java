5ad5246ca0ef7a23e91674b22597f9144b5e038f
Kedar <kedargorte77@gmail.com>
2024-02-08 17:42:55 +0530

    Entity Updates

---
+++ asset-management-project/src/main/java/com/hexaware/assetmangement/AssetManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.assetmangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagementProjectApplication.class, args);
+	}
+
+}
